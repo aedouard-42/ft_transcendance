4532cc939ce53deab75266a6f5799aa61a14f673
lina noirot <lina.noirot@gmail.com>
2023-04-06 19:53:19 +0200

    fix: null password

--- nest/backend/src/database/entities/Channel.ts
+++ nest/backend/src/database/entities/Channel.ts
@@ -47,7 +47,8 @@ export class Channel {
 
     @BeforeInsert()
     async hashPassword() {
-        if (this.password && this.password.length == 0)
+        console.log(`password: ${this.password}(${typeof(this.password)})`)
+        if (!this.password)
             this.password = null;
         else
             this.password = await bcrypt.hash(this.password, 15);
